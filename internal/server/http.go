91c8212ec99dfb8a78d8ba946de22ba3f32e7e70
Senad Uka <senaduka@gmail.com>
2018-09-30 19:59:38 +0200

    Server responds to call (mocked)

--- internal/server/http.go
+++ internal/server/http.go
@@ -3,6 +3,11 @@ package server
 import "net/http"
 
 func Start(pattern string, handler Handler) {
+
+  http.HandleFunc("/handle", func(w http.ResponseWriter, r *http.Request) {
+    stringHandler(w,r,func() string { return "OK" })
+  })
+
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     stringHandler(w,r,handler)
   })
